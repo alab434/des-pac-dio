a7d3afab2e5b5b5ebfdb84f4c18862819b6bf802
alab434 <almyr.abath@ymail.com>
2024-09-18 10:01:07 -0300

    Ajuste na variavel requirements

--- setup.py
+++ setup.py
@@ -3,20 +3,16 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     page_description = f.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name="mynomar",
     version="0.0.1",
     author="Almyr P.",
     author_email="my_email@example.com",
     description="Normaliza nomes de arquivos.",
-    readme='README.md',
     long_description=page_description,
     long_description_content_type="text/markdown",
     python_requires='>=3.10',
-    install_requires=requirements,
+    install_requires=[],
     packages=find_packages(),
     url="https://github.com/alab434/des-pac-dio",
 )
